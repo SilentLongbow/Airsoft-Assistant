71d864844d4357ce57470743d4bc648be91f689f
Matthew Kenny <mjk141@uclive.ac.nz>
2020-05-16 21:14:13 +1200

    Created an activity for deciding if the user is shown the main activity, or needs to log in / register. The transition is relatively seamless as the intent is launched before the launcher activity is finished.

---
+++ app/src/main/java/nz/ac/uclive/mjk141/airsoftloadout/LauncherActivity.kt
@@ -0,0 +1,40 @@
+package nz.ac.uclive.mjk141.airsoftloadout
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nz.ac.uclive.mjk141.airsoftloadout.utils.SIGNED_OUT
+
+/*
+ * Activity for determining which activity to start the user
+ */
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val sharedPreferences = this.getSharedPreferences(getString(R.string.stored_user_credentials),
+            Context.MODE_PRIVATE)
+        with (sharedPreferences.edit()) {
+            putLong(getString(R.string.stored_user_id_key), 1)
+            apply()
+        }
+
+        val intent = when (sharedPreferences.getLong(getString(R.string.stored_user_id_key),
+            SIGNED_OUT)) {
+            SIGNED_OUT -> registerActivityIntent()
+            else -> mainActivityIntent()
+        }
+
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                Intent.FLAG_ACTIVITY_NO_ANIMATION
+
+        startActivity(intent)
+        finish()
+    }
+
+    private fun registerActivityIntent() = Intent(this, RegisterActivity::class.java)
+
+    private fun mainActivityIntent() = Intent(this, MainActivity::class.java)
+}
---
+++ app/src/main/java/nz/ac/uclive/mjk141/airsoftloadout/utils/Constants.kt
@@ -0,0 +1,3 @@
+package nz.ac.uclive.mjk141.airsoftloadout.utils
+
+const val SIGNED_OUT: Long = -1L
